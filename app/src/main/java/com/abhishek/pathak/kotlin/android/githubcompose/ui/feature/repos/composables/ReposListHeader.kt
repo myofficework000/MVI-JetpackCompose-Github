480e798fc358f34764feb373414c14afb5dc459f
alex <mgpyaephyolay@gmail.com>
2023-06-16 16:43:43 -0500

    Added webview

--- app/src/main/java/com/abhishek/pathak/kotlin/android/githubcompose/ui/feature/repos/composables/ReposListHeader.kt
+++ app/src/main/java/com/abhishek/pathak/kotlin/android/githubcompose/ui/feature/repos/composables/ReposListHeader.kt
@@ -1,10 +1,16 @@
 package com.abhishek.pathak.kotlin.android.githubcompose.ui.feature.repos.composables
 
+import android.content.Context
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -12,6 +18,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.abhishek.pathak.kotlin.android.githubcompose.R
 import com.abhishek.pathak.kotlin.android.githubcompose.data.model.UserDetail
 import com.abhishek.pathak.kotlin.android.githubcompose.data.model.buildUserDetailPreview
@@ -19,6 +26,7 @@ import com.abhishek.pathak.kotlin.android.githubcompose.common.buildUrlIntent
 import com.abhishek.pathak.kotlin.android.githubcompose.ui.feature.common.RoundedImage
 import com.abhishek.pathak.kotlin.android.githubcompose.ui.theme.OnSurfaceBackgroundAlpha
 import com.abhishek.pathak.kotlin.android.githubcompose.ui.theme.OnSurfaceTextAlpha
+import kotlinx.coroutines.launch
 import java.util.*
 
 @Composable
@@ -90,7 +98,14 @@ fun ButtonsSession(userDetail: UserDetail) {
     // See all button
     val context = LocalContext.current
     val profileIntent = remember{ buildUrlIntent(userDetail.htmlUrl) }
-    val blogIntent = remember{ buildUrlIntent(userDetail.blogUrl) }
+    //val blogIntent = remember{ buildUrlIntent(userDetail.blogUrl) }
+    val blog = userDetail.blogUrl
+    val loadBlog = remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+
+    if(loadBlog.value){
+        LoadWebUrl(context = context, url = blog)
+    }
 
     // View Blog
     val blogNotFoundDialog = remember { mutableStateOf(false) }
@@ -109,7 +124,10 @@ fun ButtonsSession(userDetail: UserDetail) {
                 blogNotFoundDialog.value = true
             } else {
                 //Open a webview - @bitbyAlex
-                context.startActivity(blogIntent)
+                //context.startActivity(blogIntent)
+                scope.launch {
+                    loadBlog.value = true
+                }
             }
         }) {
             Text(text = stringResource(R.string.button_view_blog_title))
@@ -149,6 +167,41 @@ fun ScoreItem(count: Int, description: String) {
         )
     }
 }
+
+@Composable
+fun LoadWebUrl(context: Context, url: String){
+
+    AndroidView(factory = {
+        WebView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    })
+}
+/*
+@Composable
+fun LoadWebUrl(context: Context, url: String) {
+
+    AndroidView(factory = {
+        WebView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true // to enable youtube
+
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
+}
+ */
 
 @Preview(showBackground = true)
 @Composable
